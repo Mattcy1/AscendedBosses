26938138991d638dcda3f9dcb15fa49fa49cd392
Mattcy <140085165+Mattcy1@users.noreply.github.com>
2025-04-30 14:07:23 +0200

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,8 +2,8 @@
 
 public static class ModHelperData
 {
-    public const string WorksOnVersion = "48.0";
-    public const string Version = "1.0.0";
+    public const string WorksOnVersion = "48.2";
+    public const string Version = "1.0.1";
     public const string Name = "AscendedBosses";
 
     public const string Description = "An empty mod";
